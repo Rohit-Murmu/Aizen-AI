1121ff2c1cfef159caa6d00282ed7a66dbc8b37e
Rohit Murmu <144815102+Rohit-Murmu@users.noreply.github.com>
2024-07-08 20:17:52 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            "replicate.delivery"
+        ]
+    }
+}
+
+module.exports = nextConfig
